2b55a099343dbfdcdddfc2c8c86a7779cbb0eace
Heqiliang <heqiliang@weplus.cn>
2017-06-29 18:31:25 +0800

    update

---
+++ HQLChatBubbleView/HQLChatBubbleView/HQLChatBubbleView/View/HQLChatBubbleView.m
@@ -0,0 +1,20 @@
+//
+//  HQLChatBubbleView.m
+//  HQLChatBubbleView
+//
+//  Created by weplus on 2017/6/29.
+//  Copyright © 2017年 weplus. All rights reserved.
+//
+
+#import "HQLChatBubbleView.h"
+
+@interface HQLChatBubbleView ()
+
+@property (strong, nonatomic) CAShapeLayer *maskLayer; // layer
+
+@end
+
+@implementation HQLChatBubbleView
+
+
+@end
